a36bf6987c791a45df0b15be34944cb3b5cfeb8f
saeedyussif663 <saeedyussif663@gmail.com>
2024-09-28 22:10:29 +0000

    made the greetings into a layout

--- app/(admin)/admin/create-event/page.tsx
+++ app/(admin)/admin/create-event/page.tsx
@@ -1,11 +1,5 @@
 import CreateEventForm from '@/components/admin/CreateEventForm';
-import Greetings from '@/components/admin/Greetings';
 
 export default function CreateEvent() {
-  return (
-    <section className="px-4 py-4 font-open-sans md:px-8">
-      <Greetings />
-      <CreateEventForm />
-    </section>
-  );
+  return <CreateEventForm />;
 }
